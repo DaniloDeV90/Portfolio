9f1d7414c715f8a01fe5801b0a04b43e8a1f12db
DaniloDeV90 <duhflores78@gmail.com>
2023-05-06 14:18:38 -0300

    correcoes

--- portfolio/src/PAGES/Contato.jsx
+++ portfolio/src/PAGES/Contato.jsx
@@ -1,10 +1,16 @@
 
-
+import "./ctt.css"
 const Contato = () => {
   return (
-    <main>
+    <main id="ctt">
 
-      
+
+<div className="contatos">
+
+  <p>whtasapp</p>
+  <p>Linkedin</p>
+  
+</div>
     </main>
   )
 }
